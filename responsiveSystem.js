1df39f85d7d61894d458ed3648ac9e4d7dfd2b79
Flaromy <flaromy@foxmail.com>
2023-07-06 17:25:26 +0800

    原始值的响应式方案

--- responsiveSystem.js
+++ responsiveSystem.js
@@ -439,3 +439,48 @@ function traverse(value, seen = new Set()) {
   }
   return value
 }
+
+export function ref(val) {
+  const wrapper = { value: val }
+  Object.defineProperty(wrapper, '__v_isRef', { value: true })
+  return reactive(wrapper)
+}
+
+export function toRef(obj, key) {
+  const wrapper = {
+    get value() {
+      return obj[key]
+    },
+    set value(val) {
+      obj[key] = val
+    }
+  }
+
+  Object.defineProperty(wrapper, '__v_isRef', { value: true })
+  return wrapper
+}
+
+export function toRefs(obj) {
+  const ret = {}
+  for (const key in obj) {
+    ret[key] = toRef(obj, key)
+  }
+  return ret
+}
+
+function proxyRefs(target) {
+  return new Proxy(target, {
+    get(target, key, receiver) {
+      const value = Reflect.get(target, key, receiver)
+      return value.__v_isRef ? value.value : value
+    },
+    set(target, key, newValue, reactive) {
+      const value = target[key]
+      if (value.__v_isRef) {
+        value.value = newValue
+        return true
+      }
+      return Reflect.set(target, key, newValue, receiver)
+    }
+  })
+}
