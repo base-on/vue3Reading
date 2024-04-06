b212a2ac088a8c385ff06aec7fecc5490881164c
Flaromy <flaromy@foxmail.com>
2024-04-06 21:52:28 +0800

    9.2章节 DOM复用与key的作用

--- responsiveSystem.js
+++ responsiveSystem.js
@@ -484,3 +484,145 @@ function proxyRefs(target) {
     }
   })
 }
+
+export function createRenderer(options) {
+  const {
+    createElement,
+    insert,
+    setElementText,
+    createText,
+    setText,
+    patchProps,
+    unmount
+  } = options
+
+  const Text = Symbol()
+  const Comment = Symbol()
+  const Fragment = Symbol()
+
+  function mountElement(vnode, container) {
+    const el = vnode.el = createElement(vnode.type)
+
+    if (typeof vnode.children === 'string') {
+      setElementText(el, vnode.children)
+    } else if (Array.isArray(vnode.children)) {
+      vnode.children.forEach(child => {
+        patch(null, child, el)
+      })
+    }
+
+    if (vnode.props) {
+      for (const key in vnode.props) {
+        patchProps(el, key, null, vnode.props[key])
+      }
+    }
+
+    insert(el, container)
+  }
+
+  function patchElement(n1, n2) {
+    const el = n2.el = n1.el
+    const oldProps = n1.props
+    const newProps = n2.props
+
+    for (const key in newProps) {
+      if (newProps[key] !== oldProps[key]) {
+        patchProps(el, key, oldProps[key], newProps[key])
+      }
+    }
+
+    for (const key in oldProps) {
+      if (!(key in newProps)) {
+        patchProps(el, key, oldProps[key], null)
+      }
+    }
+
+    patchChildren(n1, n2, el)
+  }
+
+  function patchChildren(n1, n2, container) {
+    if (typeof n2.children === 'string') {
+      if (Array.isArray(n1.children)) {
+        n1.children.forEach((c) => unmount(c))
+      }
+      setElementText(container, n2.children)
+    } else if (Array.isArray(n2.children)) {
+      if (Array.isArray(n1.children)) {
+        // 这个情况是重头，需要使用Diff算法
+        const oldChildren = n1.children
+        const newChildren = n2.children
+
+        for (let i = 0; i < newChildren.length; i++) {
+          const newVnode = newChildren[i]
+
+          for (let j = 0; j < oldChildren.length; j++) {
+            const oldVnode = oldChildren[j]
+
+            if (newVnode.key === oldVnode.key) {
+              patch(oldVnode, newVnode, container)
+              break
+            }
+          }
+        }
+      } else {
+        setElementText(container, '')
+        n2.children.forEach(c => patch(null, c, container))
+      }
+    } else {
+      // 新子节点不存在
+      if (Array.isArray(n1.children)) {
+        n1.children.forEach(c => unmount(c))
+      } else if (typeof n1.children === 'string') {
+        setElementText(container, '')
+      }
+    }
+  }
+
+  function patch(n1, n2, container) {
+    if (n1 && n1.type !== n2.type) {
+      unmount(n1)
+      n1 = null
+    }
+
+    const { type } = n2
+    if (typeof type === 'string') {
+      if (!n1) {
+        mountElement(n2, container)
+      } else {
+        patchElement(n1, n2)
+      }
+    } else if (typeof type === 'object') {
+      // 是组件的情况
+    } else if (type === Text) {
+      if (!n1) {
+        const el = n2.el = createText(n2.children)
+        insert(el, container)
+      } else {
+        const el = n2.el = n1.el
+        if (n2.children !== n1.children) {
+          setText(el, n2.children)
+        }
+      }
+    } else if (type === Fragment) {
+      if (!n1) {
+        n2.children.forEach(c => patch(null, c, container))
+      } else {
+        patchChildren(n1, n2, container)
+      }
+    }
+  }
+
+  function render(vnode, container) {
+    if (vnode) {
+      patch(container._vnode, vnode, container)
+    } else {
+      if (container._vnode) {
+        unmount(container._vnode)
+      }
+    }
+
+    container._vnode = vnode
+  }
+
+  return { render }
+}
--- test.js
+++ test.js
@@ -1,18 +1,102 @@
-import { dataToResponsive, effect } from './responsiveSystem.js'
+import { createRenderer } from './responsiveSystem.js'
 
-let data = dataToResponsive({ name: 'Alice', age: 15, grade: 9 })
-
-function f1() {
-  console.log(`this student's name is ${data.name}.`)
+function shouldSetAsProps(el, key, value) {
+  if (key === 'form' && el.tagName === 'INPUT') return false
+  return key in el
 }
 
-function f2() {
-  console.log(`this student's age is ${data.age}`)
+const renderer = createRenderer({
+  createElement(tag) {
+    return document.createElement(tag)
+  },
+
+  setElementText(el, text) {
+    el.textContent = text
+  },
+
+  insert(el, parent, anchor = null) {
+    parent.insertBefore(el, anchor)
+  },
+
+  createText(text) {
+    return document.createTextNode(text)
+  },
+
+  setText(el, text) {
+    el.nodeValue = text
+  },
+
+  patchProps(el, key, prevValue, nextValue) {
+    if (/^on/.test(key)) {
+      const invokers = el._vei || (el._vei = {})
+      let invoker = invokers[key]
+      const name = key.slice(2).toLowerCase()
+      if (nextValue) {
+        if (!invoker) {
+          invoker = el._vei[key] = (e) => {
+            if (e.timeStamp < invoker.attached) return
+            if (Array.isArray(invoker.value)) {
+              invoker.value.forEach(fn => fn(e))
+            } else {
+              invoker.value(e)
+            }
+          }
+          invoker.value = nextValue
+          invoker.attached = performance.now()
+          el.addEventListener(name, invoker)
+        } else {
+          invoker.value = nextValue
+        }
+      } else if (invoker) {
+        el.removeEventListener(name, invoker)
+      }
+    } else if (key === 'class') {
+      el.className = nextValue || ''
+    } else if (shouldSetAsProps(el, key, nextValue)) {
+      const type = typeof el[key]
+      if (type === 'boolean' && nextValue === '') {
+        el[key] = true
+      } else {
+        el[key] = nextValue
+      }
+    } else {
+      el.setAttribute(key, nextValue)
+    }
+  },
+
+  unmount(vnode) {
+    if (vnode.type === Fragment) {
+      vnode.children.forEach(c => unmount(c))
+      return
+    }
+
+    const parent = vnode.el.parentNode
+    if (parent) {
+      parent.removeChild(vnode.el)
+    }
+  }
+})
+
+
+const oldVnode = {
+  type: 'div',
+  children: [
+    { type: 'p', children: '1', key: 1 },
+    { type: 'p', children: '2', key: 2 },
+    { type: 'p', children: 'hello', key: 3 }
+  ]
 }
 
-effect(f1)
-effect(f2)
+const newVnode = {
+  type: 'div',
+  children: [
+    { type: 'p', children: 'world', key: 3 },
+    { type: 'p', children: '1', key: 1 },
+    { type: 'p', children: '2', key: 2 }
+  ]
+}
+renderer.render(oldVnode, document.querySelector('#app'))
 
 setTimeout(() => {
-  data.name = 'Jane'
-}, 800);
+  renderer.render(newVnode, document.querySelector('#app'))
+}, 2000);
